36603bf550a6c0ee9262d16ac8eca09007e1c846
dsilvernail <dsilvernail@dsilvernail-VirtualBox.(none)>
2013-12-16 19:28:02 -0500

    changed layout throughout site

--- app/views/home/about.blade.php
+++ app/views/home/about.blade.php
@@ -1,53 +1,61 @@
 @extends('master')
 
 @section('content')
-	<h1>About US</h1>
+	<h1>About Us</h1>
 	
 	<div class='row'>
-		<div class='col-md-4'>
+
+		<div class='col-xs-12 col-sm-6 col=md-8'>
+			<h2 align='center'>Dan Silvernail</h2>
+			<p>Hi my name is Dan Silvernail</p>
+		</div>
+
+		<div class='col-x-6 col-md-4'>
 			<img src="pics/dan2.jpg"
 				alt='Dan Silvernail'>
 		</div>
 
-		<div class='col=md-8'>
-			<h2 align='center'>Dan Silvernail</h2>
-			<p>Hi my name is Dan Silvernail</p>
-		</div>
 	</div>
 	
 	<div class='row'>
-		<div class='col-md-4'>
-			<img src="pics/marky.jpg"
-				alt='Marky Duarte'>
-		</div>
 
-		<div class='col=md-8'>
+		<div class='col-xs-12 col-sm-6 col=md-8'>
 			<h2 align='center'>Marky Duarte</h2>
 			<p>Hi my name is Marky</p>
 		</div>
+
+		<div class='col-x-6 col-md-4'>
+			<img src="pics/marky.jpg"
+				alt='Marky Duarte'>
+		</div>
+		
 	</div>
 
 	<div class='row'>
-		<div class='col-md-4'>
+
+		<div class='col-xs-12 col-sm-6 col=md-8'>
+			<h2 align='center'>Katie Sasek</h2>
+			<p>Hi my name is Katie Sasek</p>
+		</div>
+
+		<div class='col-x-6 col-md-4'>
 			<img src="pics/katie.jpg"
 				alt='Katie Sasek'>
 		</div>
 
-		<div class='col=md-8'>
-			<h2 align='center'>Katie Sasek</h2>
-			<p>Hi my name is Katie Sasek</p>
-		</div>
 	</div>
 
 	<div class='row'>
-		<div class='col-md-4'>
-			<img src="pics/jaclyn.jpg"
-				alt='Dan Silvernail'>
-		</div>
 
-		<div class='col=md-8'>
+		<div class='col-xs-12 col-sm-6 col=md-8'>
 			<h2 align='center'>Jaclyn Ayers</h2>
 			<p>Hi my name is Jaclyn Ayers</p>
 		</div>
+
+		<div class='col-x-6 col-md-4'>
+			<img src="pics/jaclyn.jpg"
+				alt='Jaclyn Ayers'>
+		</div>
+
 	</div>
 @stop
